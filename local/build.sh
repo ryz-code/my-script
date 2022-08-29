d8d9d5a8c43b10ce2e790a315d33bfadeb5ced7f
ryzXD <111454488+ryz-code@users.noreply.github.com>
2022-08-29 09:36:29 +0700

    Update build.sh

--- local/build.sh
+++ local/build.sh
@@ -1,186 +1,234 @@
 #!/usr/bin/env bash
-# Copyright ©2022 ryz-code
+# Copyright ©2022 XSans02
 
 # Function to show an informational message
-msg() {
+function msg() {
     echo -e "\e[1;32m$*\e[0m"
 }
 
-err() {
+function panel() {
+    echo -e "\e[1;34m$*\e[0m"
+}
+
+function panel2() {
+    echo -ne "\e[1;34m$*\e[0m"
+}
+
+function err() {
     echo -e "\e[1;31m$*\e[0m"
 }
 
+# Home directory
+HOME=$(pwd)
+
 # Cancel if something is missing
-if [[ -z "${TELEGRAM_TOKEN}" ]] || [[ -z "${TELEGRAM_CHAT}" ]] || [[ -z "${TC}" ]]; then
-    err "* There is something missing!"
+msg "* Token Checker"
+sleep 3
+if [[ -z "${TELEGRAM_TOKEN}" ]] || [[ -z "${CHANNEL_ID}" ]] || [[ -z "${GIT_TOKEN}" ]]; then
+    err "(X) There is something missing!"
     exit
+else
+    msg "(Y) Everything is okey"
+    msg ""
 fi
 
-# We only run this when it's not running on GitHub Actions
-if [[ -z ${GITHUB_ACTIONS:-} ]]; then
-    rm -rf kernel
-    git clone --depth=1 -b "$1" https://github.com/a3-Prjkt/kernel_xiaomi_ginkgo_consistenX kernel
-    cd kernel || exit
-fi
-
-# Clone AnyKernel3 source
-msg "* Clone AnyKernel3 source"
-git clone --depth=1 -b master https://github.com/ryz-code/AnyKernel3 AK3
-
-# Clone toolchain source
-if [[ "${TC}" == "weebx15" ]]; then
-    msg "* Clone WeebX Clang 15.x good revision"
-    git clone --depth=1 -b release/15-gr --depth=1 https://gitlab.com/XSans0/weebx-clang.git clang
-fi
-
-# Setup
-KERNEL_DIR="$PWD"
-KERNEL_IMG="$KERNEL_DIR/out/arch/arm64/boot/Image"
-KERNEL_DTBO="$KERNEL_DIR/out/arch/arm64/boot/dtbo.img"
-KERNEL_LOG="$KERNEL_DIR/out/log-$(TZ=Asia/Jakarta date +'%H%M').txt"
-AK3_DIR="$KERNEL_DIR/AK3"
-DEVICE="ginkgo"
-CORES="$(nproc --all)"
-CPU="$(lscpu | sed -nr '/Model name/ s/.*:\s*(.*) */\1/p')"
-BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-COMMIT="$(git log --pretty=format:'%s' -1)"
-
-# Toolchain setup
-if
-    CLANG_DIR="$KERNEL_DIR/clang"
+# Clang source checker
+msg ""
+msg "* Clang Checker"
+sleep 3
+if [[ "$(cat toolchain.txt)" == "clang" ]]; then
+    # Toolchain directory
+    CLANG_DIR="$HOME/clang"
     PrefixDir="$CLANG_DIR/bin/"
     ARM64="aarch64-linux-gnu-"
     ARM32="arm-linux-gnueabi-"
     TRIPLE="aarch64-linux-gnu-"
-    COMPILE="clang"
-    PATH="$CLANG_DIR/bin:$PATH"
-    KBUILD_COMPILER_STRING="$("${CLANG_DIR}"/bin/clang --version | head -n 1 | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+    export PATH="$CLANG_DIR/bin:$PATH"
+    export KBUILD_COMPILER_STRING="$(${CLANG_DIR}/bin/clang --version | head -n 1 | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+
+    msg "(Y) ${KBUILD_COMPILER_STRING} detected."
+elif [[ "$(cat toolchain.txt)" == "aosp" ]]; then
+    # Toolchain directory
+    CLANG_DIR="$HOME/clang"
+    GCC64_DIR="$HOME/arm64"
+    GCC32_DIR="$HOME/arm32"
+    PrefixDir="$CLANG_DIR/bin/"
+    ARM64="aarch64-linux-android-"
+    ARM32="arm-linux-androideabi-"
+    TRIPLE="aarch64-linux-gnu-"
+    export PATH="$CLANG_DIR/bin:$GCC64_DIR/bin:$GCC32_DIR/bin:$PATH"
+    export KBUILD_COMPILER_STRING="$(${CLANG_DIR}/bin/clang --version | head -n 1 | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+
+    msg "(Y) ${KBUILD_COMPILER_STRING} detected."
+elif [[ "$(cat toolchain.txt)" == "sdclang" ]]; then
+    # Toolchain directory
+    CLANG_DIR="$HOME/clang/compiler"
+    GCC64_DIR="$HOME/arm64"
+    GCC32_DIR="$HOME/arm32"
+    PrefixDir="$CLANG_DIR/bin/"
+    ARM64="aarch64-linux-android-"
+    ARM32="arm-linux-androideabi-"
+    TRIPLE="aarch64-linux-gnu-"
+    export PATH="$CLANG_DIR/bin:$GCC64_DIR/bin:$GCC32_DIR/bin:$PATH"
+    export KBUILD_COMPILER_STRING="$(${CLANG_DIR}/bin/clang --version | head -n 1 | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+
+    msg "(Y) ${KBUILD_COMPILER_STRING} detected."
+else
+    err "(X) Toolchain not found, please clone first!"
+    msg ""
+    msg "* Load toolchain source"
+    sleep 10
+    source toolchain.sh
 fi
+
+# Clone Source
+msg ""
+msg "* Clone Kernel/AK3 Source"
+rm -rf kernel AK3
+git clone --depth=1 -b twelve https://github.com/a3-Prjkt/kernel_xiaomi_ginkgo_consistenX kernel
+git clone --depth=1 -b master https://github.com/ryz-code/AnyKernel3 AK3
+cd kernel || exit
+
+# environtment
+KERNEL_DIR="$PWD"
+KERNEL_IMG="$KERNEL_DIR/out/arch/arm64/boot/Image"
+KERNEL_DTBO="$KERNEL_DIR/out/arch/arm64/boot/dtbo.img"
+KERNEL_LOG="$KERNEL_DIR/out/log-$(TZ=Asia/Jakarta date +'%Y%m%d').txt"
+AK3_DIR="$HOME/AK3/"
+BASE_DTB_NAME="sm8150-v2"
+CODENAME="ginkgo"
+DEFCONFIG="vendor/ginkgo-perf_defconfig"
+CORES=$(nproc --all)
+CPU=$(lscpu | sed -nr '/Model name/ s/.*:\s*(.*) */\1/p')
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+COMMIT="$(git log --pretty=format:'%s' -1)"
+
+# Set default time to WIB
+export TZ="Asia/Jakarta"
+export ZIP_DATE="$(TZ=Asia/Jakarta date +'%Y%m%d')"
+export CURRENTDATE="$(TZ=Asia/Jakarta date +"%A, %d %b %Y, %H:%M:%S")"
 
 # Export
-export TZ="Asia/Jakarta"
 export ARCH="arm64"
 export SUBARCH="arm64"
-export KBUILD_BUILD_USER="ryzXD.ツ"
-export KBUILD_BUILD_HOST="Ryz-Server"
-export BOT_MSG_URL="https://api.telegram.org/bot$BOT_TOKEN/sendMessage"
-export BOT_BUILD_URL="https://api.telegram.org/bot$BOT_TOKEN/sendDocument"
-export PATH
-export KBUILD_COMPILER_STRING
+export KBUILD_BUILD_USER="XSansツ"
+export KBUILD_BUILD_HOST="Wibu-Server"
 
-# Telegram Setup
-git clone --depth=1 https://github.com/XSans0/Telegram Telegram
+# Include files
+source $HOME/telegram.sh
 
-TELEGRAM="$KERNEL_DIR/Telegram/telegram"
-send_msg() {
-  "${TELEGRAM}" -H -D \
-      "$(
-          for POST in "${@}"; do
-              echo "${POST}"
-          done
-      )"
-}
+# Menu
+while true; do
+    panel ""
+    panel " Menu                                                               "
+    panel " ╔═════════════════════════════════════════════════════════════════╗"
+    panel " ║ 1. Start Compile With Clang                                     ║"
+    panel " ║ 2. Copy Image, dtbo, dtb to Flashable Dir                       ║"
+    panel " ║ 3. Make Zip                                                     ║"
+    panel " ║ 4. Upload to Telegram                                           ║"
+    panel " ║ e. Back Main Menu                                               ║"
+    panel " ╚═════════════════════════════════════════════════════════════════╝"
+    panel2 " Enter your choice 1-4, or press 'e' for back to Main Menu : "
 
-send_file() {
-    "${TELEGRAM}" -H \
-    -f "$1" \
-    "$2"
-}
+    read -r menu
 
-start_msg() {
-    send_msg "<b>New Kernel On The Way</b>" \
-                 "<b>==================================</b>" \
-                 "<b>Device : </b>" \
-                 "<code>* $DEVICE</code>" \
-                 "<b>Branch : </b>" \
-                 "<code>* $BRANCH</code>" \
-                 "<b>Build Using : </b>" \
-                 "<code>* $CPU $CORES thread</code>" \
-                 "<b>Last Commit : </b>" \
-                 "<code>* $COMMIT</code>" \
-                 "<b>==================================</b>"
-}
+    # Start Compile With Clang
+    if [[ "$menu" == "1" ]]; then
+        msg ""
+        START=$(date +"%s")
+        msg "(Y) Start Compile kernel for $CODENAME using $CPU $CORES thread"
+        msg ""
+        start_msg
 
-# Start compile
-START=$(date +"%s")
-msg "* Start Compile kernel for $DEVICE using $CPU $CORES thread"
-start_msg
+        # Run Build
+        make -j"$CORES" O=out "${DEFCONFIG}" \
+            CC=${PrefixDir}clang \
+            LD=${PrefixDir}ld.lld \
+            AR=${PrefixDir}llvm-ar \
+            NM=${PrefixDir}llvm-nm \
+            HOSTCC=${PrefixDir}clang \
+            HOSTCXX=${PrefixDir}clang++ \
+            STRIP=${PrefixDir}llvm-strip \
+            OBJCOPY=${PrefixDir}llvm-objcopy \
+            OBJDUMP=${PrefixDir}llvm-objdump \
+            READELF=${PrefixDir}llvm-readelf \
+            OBJSIZE=${PrefixDir}llvm-size \
+            STRIP=${PrefixDir}llvm-strip \
+            CLANG_TRIPLE=${TRIPLE} \
+            CROSS_COMPILE=${ARM64} \
+            CROSS_COMPILE_COMPAT=${ARM32} \
+            LLVM=1 2>&1 | tee ${KERNEL_LOG}
 
-if [[ "${COMPILE}" == "clang" ]]; then
-    make O=out "vendor/$DEVICE"-perf_defconfig
-    make -j"$CORES" O=out \
-        CC="${PrefixDir}"clang \
-        LD="${PrefixDir}"ld.lld \
-        AR="${PrefixDir}"llvm-ar \
-        NM="${PrefixDir}"llvm-nm \
-        HOSTCC="${PrefixDir}"clang \
-        HOSTCXX="${PrefixDir}"clang++ \
-        STRIP="${PrefixDir}"llvm-strip \
-        OBJCOPY="${PrefixDir}"llvm-objcopy \
-        OBJDUMP="${PrefixDir}"llvm-objdump \
-        READELF="${PrefixDir}"llvm-readelf \
-        OBJSIZE="${PrefixDir}"llvm-size \
-        STRIP="${PrefixDir}"llvm-strip \
-        CLANG_TRIPLE=${TRIPLE} \
-        CROSS_COMPILE=${ARM64} \
-        CROSS_COMPILE_COMPAT=${ARM32} \
-        CROSS_COMPILE_ARM32=${ARM32} \
-        LLVM=1 2>&1 | tee "${KERNEL_LOG}"
+        if ! [ -a "$KERNEL_IMG" ]; then
+            err ""
+            err "(X) Compile Kernel for $CODENAME failed, See buildlog to fix errors"
+            err ""
+            send_log "<b>Build Failed, See log to fix errors</b>"
+            exit
+        fi
 
-    if [[ -f "$KERNEL_IMG" ]]; then
-        msg "* Compile Kernel for $DEVICE successfully."
-    else
-        err "* Compile Kernel for $DEVICE failed, See buildlog to fix errors"
-        send_file "$KERNEL_LOG" "<b>Compile Kernel for $DEVICE failed, See buildlog to fix errors</b>"
+        END=$(date +"%s")
+        TOTAL_TIME=$(("$END" - "$START"))
+        msg ""
+        msg "(Y) Compile Kernel for $CODENAME successfully."
+        msg "(Y) Total time elapsed: $(("$TOTAL_TIME" / 60)) Minutes, $(("$TOTAL_TIME" % 60)) Second."
+        msg ""
+        end_msg
+    fi
+
+    # Copy image, dtbo, dtb to flashable dir
+    if [[ "$menu" == "2" ]]; then
+        msg ""
+        msg "* Copying Image, dtbo, dtb ..."
+        if [[ -f "$KERNEL_IMG" ]]; then
+            cp "$KERNEL_IMG" "$AK3_DIR"/
+            msg "(Y) Copy kernel image successfully"
+        else
+            err "(X) Image Not Found"
+        fi
+        sleep 1
+        if [[ -f "$KERNEL_DTBO" ]]; then
+            cp "$KERNEL_DTBO" "$AK3_DIR"/
+            msg "(Y) Copy dtbo successfully"
+        else
+            err "(X) dtbo Not Found"
+        fi
+        sleep 1
+        if [[ -f $KERNEL_DTB/${BASE_DTB_NAME}.dtb ]]; then
+		    cp "$KERNEL_DTB/${BASE_DTB_NAME}.dtb" "$AK3_DIR/dtb.img"
+            msg "(Y) Copy dtb successfully"
+            msg ""
+        else
+            err "(X) dtb Not Found"
+            err ""
+        fi
+    fi
+
+    # Make Zip
+    if [[ "$menu" == "3" ]]; then
+        cd "$AK3_DIR" || exit
+        ZIP_NAME=["$ZIP_DATE"]WeebX-Personal-"$(TZ=Asia/Jakarta date +'%H%M')".zip
+        zip -r9 "$ZIP_NAME" ./*
+        cd "$KERNEL_DIR" || exit
+
+        msg ""
+        msg "(Y) Zipping Kernel successfully"
+        msg ""
+    fi
+
+    # Upload Telegram
+    if [[ "$menu" == "4" ]]; then
+        send_log "<b>Build Successfully</b>"
+        send_file "<b>md5 : </b><code>$(md5sum "$AK3_DIR/$ZIP_NAME" | cut -d' ' -f1)</code>"
+
+        msg ""
+	    msg "(Y) Upload to telegram successfully"
+        msg ""
+    fi
+
+    # Exit
+    if [[ "$menu" == "e" ]]; then
         exit
     fi
-    
-    if [[ -f "$KERNEL_IMG" ]]; then
-        msg "* Compile Kernel for $DEVICE successfully."
-    else
-        err "* Compile Kernel for $DEVICE failed, See buildlog to fix errors"
-        send_file "$KERNEL_LOG" "<b>Compile Kernel for $DEVICE failed, See buildlog to fix errors</b>"
-        exit
-    fi
-fi
 
-# End compile
-END=$(date +"%s")
-TOTAL_TIME=$(("END" - "START"))
-export START END TOTAL_TIME
-msg "* Total time elapsed: $(("TOTAL_TIME" / 60)) Minutes, $(("TOTAL_TIME" % 60)) Second."
-
-# Copy Image, dtbo, dtb
-if [[ -f "$KERNEL_IMG" ]] || [[ -f "$KERNEL_DTBO" ]] || [[ -f "$KERNEL_DTB" ]]; then
-    cp "$KERNEL_IMG" "$AK3_DIR"
-    cp "$KERNEL_DTBO" "$AK3_DIR"
-	cp "$KERNEL_DTB" "$AK3_DIR/dtb.img"
-    msg "* Copy Image, dtbo, dtb successfully"
-else
-    err "* Copy Image, dtbo, dtb failed!"
-    exit
-fi
-
-# Zip
-cd "$AK3_DIR" || exit
-ZIP_DATE="$(TZ=Asia/Jakarta date +'%Y%m%d')"
-ZIP_DATE2="$(TZ=Asia/Jakarta date +'%H%M')"
-ZIP_NAME=["$ZIP_DATE"]Ryz-Personal-"$ZIP_DATE2".zip
-zip -r9 "$ZIP_NAME" ./*
-
-# Upload build
-send_file "$KERNEL_LOG" "<b>Compile Kernel for $DEVICE successfully.</b>"
-send_file "$AK3_DIR/$ZIP_NAME" "
-<b>Build Successfully</b>
-<b>============================</b>
-<b>Build Date : </b>
-<code>* $(date +"%A, %d %b %Y")</code>
-<b>Build Took : </b>
-<code>* $(("TOTAL_TIME" / 60)) Minutes, $(("TOTAL_TIME" % 60)) Second.</code>
-<b>Linux Version : </b>
-<code>* v$(grep Linux "$KERNEL_DIR"/out/.config | cut -f 3 -d " ")</code>
-<b>Md5 : </b>
-<code>* $(md5sum "$AK3_DIR/$ZIP_NAME" | cut -d' ' -f1)</code>
-<b>Compiler : </b>
-<code>* $KBUILD_COMPILER_STRING</code>
-<b>============================</b>"
+done
